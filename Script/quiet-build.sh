a78cba551992e780ee2d270af30de2857c044b12
bijanbina <bijanbina@gmail.com>
2016-04-15 17:35:11 +0430

    make it to package

--- Script/quiet-build.sh
+++ Script/quiet-build.sh
@@ -1,8 +1,5 @@
 #! /bin/sh
-FILENAME=milestone
-xelatex -shell-escape -interaction=nonstopmode -file-line-error $FILENAME.tex | grep ".*:[0-9]*" 
-if [ -e $FILENAME.pdf ]; then
-	evince $FILENAME.pdf
-else
-	exit 10
-fi
+#FILENAME=milestone
+#xelatex -shell-escape -interaction=nonstopmode -file-line-error $FILENAME.tex | grep ".*:[0-9]*" 
+xelatex -shell-escape -interaction=nonstopmode -file-line-error "$1" | grep ".*:[0-9]*" | grep -vwE "Package"
+
