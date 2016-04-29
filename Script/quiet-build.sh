32604478bfb2194e4f58ee67de98775fedabfb0b
bijanbina <bijanbina@gmail.com>
2016-04-29 19:00:10 +0430

    auto ToC generate

--- Script/quiet-build.sh
+++ Script/quiet-build.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 #FILENAME=milestone
 #xelatex -shell-escape -interaction=nonstopmode -file-line-error $FILENAME.tex | grep ".*:[0-9]*" 
+#check for double compile if 
+#TOC is not existed already
+baseName=`echo "$1" | awk -F "." '{print $1}'`
+if [ ! -e "$baseName.toc" ]; then
+	echo "Generating ToC"
+	xelatex -shell-escape -interaction=nonstopmode -file-line-error "$1" > /dev/null 2>&1
+fi
+
 xelatex -shell-escape -interaction=nonstopmode -file-line-error "$1" | grep ".*:[0-9]*" | grep -vwE "Package"
 
